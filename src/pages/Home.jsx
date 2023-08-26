8c719201c1746c35d919b84607ca5efc70339c35
Sabit <salfisabit@gmail.com>
2023-08-27 03:09:57 +0600

    Changed the Welcome Message a bit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
+import "./Home.css";
+
 import Nav from "../components/Nav/Nav";
 
 const Home = () => {
   return (
     <div>
       <Nav />
-      Welcome to Home
+      <h3 className="temp-welcome-message">WELCOME TO TODO HIVE.</h3>
     </div>
   );
 };
