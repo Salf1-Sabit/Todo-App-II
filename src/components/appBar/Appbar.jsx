4eafe86db9a6ee6480fa5d581dc8de499c9e48e7
Salfi Sabit <70028517+Salf1-Sabit@users.noreply.github.com>
2023-10-07 08:11:56 +0600

    Added tostify and almost completed the todo component

--- src/components/appBar/Appbar.jsx
+++ src/components/appBar/Appbar.jsx
@@ -5,7 +5,16 @@ import { NavLink } from "react-router-dom";
 
 import Person1 from "../../assets/images/person-2.jpg";
 
-import { Drawer, Box, AppBar, Toolbar, Badge, Tooltip } from "@mui/material";
+import {
+  Drawer,
+  Box,
+  AppBar,
+  Toolbar,
+  Badge,
+  Tooltip,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
 import { Avatar } from "@mui/material";
 
 // MUI ITEMS
@@ -32,6 +41,21 @@ import AddIcon from "@mui/icons-material/Add";
 const drawerWidth = 240;
 
 const Appbar2 = (props) => {
+  // MUI THEME
+  const theme = createTheme({
+    palette: {
+      primary: {
+        light: "#7780e8",
+        main: "#5763e3",
+        dark: "#3545dc",
+        contrastText: "#fff",
+      },
+      secondary: {
+        main: "#e35763",
+      },
+    },
+  });
+
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -61,7 +85,7 @@ const Appbar2 = (props) => {
           <ListItem disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                <TodayIcon sx={{ color: "#5762E3" }} />
+                <TodayIcon color="success" />
               </ListItemIcon>
               <ListItemText primary={"Today"} />
             </ListItemButton>
@@ -71,7 +95,7 @@ const Appbar2 = (props) => {
         <ListItem disablePadding>
           <ListItemButton>
             <ListItemIcon>
-              <UpcomingIcon sx={{ color: "#5762E3" }} />
+              <UpcomingIcon color="secondary" />
             </ListItemIcon>
             <ListItemText primary={"Upcoming"} />
           </ListItemButton>
@@ -84,7 +108,7 @@ const Appbar2 = (props) => {
         <ListItem disablePadding>
           <ListItemButton>
             <ListItemIcon>
-              <WorkspacesIcon sx={{ color: "#5762E3" }} />
+              <WorkspacesIcon sx={{ color: "#ff6f00" }} />
             </ListItemIcon>
             <ListItemText primary={"Personal"} />
           </ListItemButton>
@@ -93,7 +117,7 @@ const Appbar2 = (props) => {
         <ListItem disablePadding>
           <ListItemButton>
             <ListItemIcon>
-              <GroupsIcon sx={{ color: "#5762E3" }} />
+              <GroupsIcon sx={{ color: "#6200ea" }} />
             </ListItemIcon>
             <ListItemText primary={"Team"} />
           </ListItemButton>
@@ -106,122 +130,124 @@ const Appbar2 = (props) => {
     window !== undefined ? () => window().document.body : undefined;
   return (
     <>
-      <AppBar
-        position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-          backgroundColor: "#5762E3",
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-        <div>
-          <Tooltip title="Add task">
-            <IconButton sx={{ marginLeft: 1, color: "#fff" }}>
-              <AddIcon />
-            </IconButton>
-          </Tooltip>
-
-          <Tooltip title="Messages">
-            <IconButton sx={{ marginLeft: 1, color: "#fff" }}>
-              <Badge color="secondary" badgeContent={3}>
-                <MessageIcon />
-              </Badge>
-            </IconButton>
-          </Tooltip>
-
-          <Tooltip title="Notifications">
-            <IconButton sx={{ marginLeft: 1, color: "#fff" }}>
-              <Badge color="secondary" badgeContent={17}>
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-          </Tooltip>
-
-          <Tooltip title="Profile">
+      <ThemeProvider theme={theme}>
+        <AppBar
+          position="fixed"
+          sx={{
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+            ml: { sm: `${drawerWidth}px` },
+            backgroundColor: "#5762E3",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Toolbar>
             <IconButton
-              aria-controls={open ? "basic-menu" : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
-              onClick={handleClick}
-              sx={{ marginLeft: 1, marginRight: 1, color: "#fff" }}
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
             >
-              <Avatar
-                alt="Remy Sharp"
-                src={Person1}
-                sx={{ width: 32, height: 32 }}
-              />
+              <MenuIcon />
             </IconButton>
-          </Tooltip>
+          </Toolbar>
+          <div>
+            <Tooltip title="Add task">
+              <IconButton sx={{ marginLeft: 1, color: "#fff" }}>
+                <AddIcon />
+              </IconButton>
+            </Tooltip>
 
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-button",
+            <Tooltip title="Messages">
+              <IconButton sx={{ marginLeft: 1, color: "#fff" }}>
+                <Badge color="secondary" badgeContent={3}>
+                  <MessageIcon />
+                </Badge>
+              </IconButton>
+            </Tooltip>
+
+            <Tooltip title="Notifications">
+              <IconButton sx={{ marginLeft: 1, color: "#fff" }}>
+                <Badge color="secondary" badgeContent={17}>
+                  <NotificationsIcon />
+                </Badge>
+              </IconButton>
+            </Tooltip>
+
+            <Tooltip title="Profile">
+              <IconButton
+                aria-controls={open ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+                sx={{ marginLeft: 1, marginRight: 1, color: "#fff" }}
+              >
+                <Avatar
+                  alt="Remy Sharp"
+                  src={Person1}
+                  sx={{ width: 32, height: 32 }}
+                />
+              </IconButton>
+            </Tooltip>
+
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+              <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
+          </div>
+        </AppBar>
+
+        <Box
+          component="nav"
+          sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+          aria-label="mailbox folders"
+        >
+          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
             }}
           >
-            <MenuItem onClick={handleClose}>Profile</MenuItem>
-            <MenuItem onClick={handleClose}>My account</MenuItem>
-            <MenuItem onClick={handleClose}>Logout</MenuItem>
-          </Menu>
-        </div>
-      </AppBar>
-
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
-      >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: "none", sm: "block" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
-      </Box>
+            {drawer}
+          </Drawer>
+          <Drawer
+            variant="permanent"
+            sx={{
+              display: { xs: "none", sm: "block" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
+            }}
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+      </ThemeProvider>
     </>
   );
 };
--- src/pages/todoApp/TodoApp.jsx
+++ src/pages/todoApp/TodoApp.jsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import "./todoApp.css";
 
 import {
+  Alert,
   Box,
   Divider,
   IconButton,
@@ -12,19 +13,30 @@ import {
   ListItemIcon,
   ListItemText,
   Menu,
+  TextField,
 } from "@mui/material";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Toolbar from "@mui/material/Toolbar";
 
 import Appbar from "../../components/appBar/Appbar";
 
 import TuneIcon from "@mui/icons-material/Tune";
-import Checkbox from "@mui/material/Checkbox";
 import LayersIcon from "@mui/icons-material/Layers";
 import SortIcon from "@mui/icons-material/Sort";
 
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import { useState } from "react";
+
+import Snackbar from "@mui/material/Snackbar";
+
 const drawerWidth = 240;
 
+// CURRENT DATE
 const month = [
   "January",
   "February",
@@ -50,11 +62,28 @@ const weekday = [
   "Saturday",
 ];
 
-const date = new Date();
+let date = new Date();
+let curDate = date.getDate();
 let mon = date.getMonth();
-let day = date.getDay();
+let weekDay = date.getDay();
 
 function TodoApp() {
+  // Snackbar Toggle
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+
+  const handleSaveClick = () => {
+    setSnackbarOpen(true);
+  };
+
+  const handleSnackbarClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setSnackbarOpen(false);
+  };
+
+  // MENU TOGGLE
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
@@ -64,88 +93,165 @@ function TodoApp() {
     setAnchorEl(null);
   };
 
+  // MUI THEME
+  const theme = createTheme({
+    palette: {
+      primary: {
+        light: "#7780e8",
+        main: "#5763e3",
+        dark: "#3545dc",
+        contrastText: "#fff",
+      },
+    },
+  });
+
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <Appbar />
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Toolbar />
-        <div className="todo-parent-container">
-          <div className="page-header">
-            <div className="page-heading">
-              <h3>
-                Today
-                <span className="current-date">
-                  <span>{" " + weekday[day].substring(0, 3) + " "}</span>
-                  <span>{day}</span>
-                  <span>{" " + month[mon].substring(0, 3) + " "}</span>
-                </span>
-              </h3>
-            </div>
-
-            <IconButton
-              onClick={handleClick}
-              aria-controls={open ? "basic-menu" : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
-            >
-              <TuneIcon />
-            </IconButton>
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              MenuListProps={{
-                "aria-labelledby": "basic-button",
-              }}
-            >
-              <List>
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemIcon>
-                      <SortIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Sorting" fontSize="small" />
-                  </ListItemButton>
-                </ListItem>
-
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemIcon>
-                      <LayersIcon fontSize="small" />
-                    </ListItemIcon>
-                    <ListItemText primary="Grouping" />
-                  </ListItemButton>
-                </ListItem>
-              </List>
-            </Menu>
-          </div>
-
-          <Divider />
-          <div className="todo-child-container">
-            <div className="todo-item">
-              <div className="todo-item-header">
-                <Checkbox size="small" color="success" />
-                <div>Heading</div>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <Appbar />
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            p: 3,
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Toolbar />
+          <div className="todo-parent-container">
+            <div className="page-header">
+              <div className="page-heading">
+                <h3>
+                  Today
+                  <span className="current-date">
+                    <span>{" " + weekday[weekDay].substring(0, 3) + " "}</span>
+                    <span>{curDate}</span>
+                    <span>{" " + month[mon].substring(0, 3) + " "}</span>
+                  </span>
+                </h3>
               </div>
-              <div>hello</div>
+
+              <IconButton
+                onClick={handleClick}
+                aria-controls={open ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+              >
+                <TuneIcon />
+              </IconButton>
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  "aria-labelledby": "basic-button",
+                }}
+              >
+                <List>
+                  <ListItem disablePadding>
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <SortIcon />
+                      </ListItemIcon>
+                      <ListItemText primary="Sorting" fontSize="small" />
+                    </ListItemButton>
+                  </ListItem>
+
+                  <ListItem disablePadding>
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <LayersIcon fontSize="small" />
+                      </ListItemIcon>
+                      <ListItemText primary="Grouping" />
+                    </ListItemButton>
+                  </ListItem>
+                </List>
+              </Menu>
+            </div>
+
+            <Divider />
+
+            <div className="todo-child-container">
+              <Card
+                sx={{
+                  border: 1,
+                  borderColor: "grey.300",
+                  maxWidth: "100%",
+                  marginTop: "1rem",
+                  boxShadow: 0,
+                }}
+              >
+                <CardContent>
+                  <div className="todo-item-header">
+                    <div
+                      style={{
+                        fontSize: "16px",
+                        fontWeight: "400",
+                      }}
+                    ></div>
+                    <TextField
+                      id="standard-basic"
+                      label="Title"
+                      variant="standard"
+                    />
+                    <TextField
+                      id="standard-basic"
+                      label="Description"
+                      variant="standard"
+                    />
+                  </div>
+                </CardContent>
+
+                <Divider />
+
+                <div className="card-bottom">
+                  <div className="card-bottom-left">
+                    <CardActions>
+                      <Button size="small">Share</Button>
+                      <Button size="small">Learn More</Button>
+                    </CardActions>
+                  </div>
+                  <div className="card-bottom-right">
+                    <CardActions>
+                      <Button size="small" sx={{ color: "#5762E3" }}>
+                        Cancel
+                      </Button>
+                      <Button
+                        variant="contained"
+                        size="small"
+                        color="primary"
+                        onClick={handleSaveClick}
+                      >
+                        Save
+                      </Button>
+                      <Snackbar
+                        open={snackbarOpen}
+                        autoHideDuration={6000}
+                        onClose={handleSnackbarClose}
+                      >
+                        <Alert
+                          onClose={handleSnackbarClose}
+                          severity="success"
+                          sx={{ width: "100%" }}
+                        >
+                          Your task is successfully added!
+                        </Alert>
+                      </Snackbar>
+                    </CardActions>
+                  </div>
+                </div>
+              </Card>
             </div>
           </div>
-        </div>
+        </Box>
       </Box>
-    </Box>
+    </ThemeProvider>
   );
 }
 export default TodoApp;
